7d484899bfc8dcd570b6b5369d88210142cda37d
witchlad <dolearczyk95@gmail.com>
2020-11-22 10:28:00 +0100

    Updated files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let button = document.getElementById("hamburger")
 let navigation = document.getElementById("list")
+
 button.addEventListener("click",function(){
     button.classList.toggle("change");
     navigation.classList.toggle("open")
-    console.log(navigation)
 })
